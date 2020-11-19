616fe56001c720b3def6374c6ce0e7255f9345aa
sarathy2511 <sarathy2511@gmail.com>
2020-11-19 16:36:10 +0530

    success

---
+++ src/org/test/Employee.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Employee {
+private void empId() {
+	// TODO Auto-generated method stub
+System.out.println("method");
+}
+public Employee() {
+System.out.println("constructor");	// TODO Auto-generated constructor stub
+}
+public static void main(String[] args) {
+	
+
+Employee e = new Employee();
+
+
+}
+}
